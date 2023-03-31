0cd52bed5fe9b18866a088d1c824ac49a070d077
Ferran Bals <fbals@uoc.edu>
2023-03-31 08:58:20 +0200

    Requests checkbox added + translations

--- src/components/TableParent.vue
+++ src/components/TableParent.vue
@@ -44,28 +44,56 @@
       <!-- FIRST TAB END -->
 
       <!-- SECOND TAB -->
+
       <!-- Enrollments Table -->
       <EnrollmentTable v-if="tab === 'payments'" :enrollments="enrollments" />
       <!-- SECOND TAB END -->
 
       <!-- THIRD TAB -->
+
+      <div class="dropdown" data-control="checkbox-dropdown">
+        <div class="dropdown-list">
+          <label class="dropdown-option">
+            <input type="checkbox" v-model="aep" checked />
+            {{ $t("AEPRequests.sectionTitle") }}
+          </label>
+
+          <label class="dropdown-option">
+            <input type="checkbox" v-model="certs" checked />
+            {{ $t("certificates.sectionTitle") }}
+          </label>
+
+          <label class="dropdown-option">
+            <input type="checkbox" v-model="virtualTestRequest" checked />
+            {{ $t("virtualTestRequests.sectionTitle") }}
+          </label>
+
+          <label class="dropdown-option">
+            <input type="checkbox" v-model="itineraryRequest" checked />
+            {{ $t("itineraryRequests.sectionTitle") }}
+          </label>
+        </div>
+      </div>
       <!-- AEP Requests Table -->
-      <AEPRequestsTable v-if="tab === 'requests'" :AEPRequests="AEPRequests" />
+      <AEPRequestsTable
+        v-if="tab === 'requests' && aep"
+        :AEPRequests="AEPRequests"
+      />
 
       <!-- Certificates Table -->
       <CertificatesTable
-        v-if="tab === 'requests'"
+        v-if="tab === 'requests' && certs"
         :certificates="certificates"
       />
 
       <!-- Virtual test requests Table -->
       <VirtualTestRequestTable
-        v-if="tab === 'requests'"
+        v-if="tab === 'requests' && virtualTestRequest"
         :virtualTestRequests="virtualTestRequests"
       />
       <!-- Itineary Table -->
       <ItineraryRequestsTable
-        v-if="tab === 'requests'"
+        v-if="tab === 'requests' && itineraryRequest"
         :itineraryRequests="itineraryRequests"
       />
       <!-- THIRD TAB END -->
@@ -80,6 +108,7 @@ export default {
 </script>
 
 <script setup>
+import { ref } from "vue";
 import SubjectTable from "./tables/SubjectTable.vue";
 import CreditsTable from "./tables/CreditsTable.vue";
 import EnrollmentTable from "./tables/EnrollmentTable.vue";
@@ -87,9 +116,13 @@ import AEPRequestsTable from "./tables/AEPRequestsTable.vue";
 import CertificatesTable from "./tables/CertificatesTable.vue";
 import VirtualTestRequestTable from "./tables/VirtualTestRequestsTable.vue";
 import ItineraryRequestsTable from "./tables/ItineraryRequestsTable.vue";
-import { ref } from "vue";
 
 const tab = ref("subject");
+
+const aep = ref(true);
+const certs = ref(true);
+const virtualTestRequest = ref(true);
+const itineraryRequest = ref(true);
 
 defineProps({
   itineraryRequests: Object,
